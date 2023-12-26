79831a9c25e8ee0719d28c48f9362eb07189062f
Sparsh Goel <sparshgoelt26@gmail.com>
2023-12-26 23:36:23 +0530

    feat: Added Searchform component and styleing for same

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './components/Banner';
 import Header from './components/Header';
+import SearchForm from './components/SearchForm';
 
 function App() {
   return (
     <>
     <Header />
     <Banner />
+    <SearchForm />
     </>
   );
 }
